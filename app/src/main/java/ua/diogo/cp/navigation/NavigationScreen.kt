69bb02409014c1e6363d4bcbfbc1e0c9819cc2a8
falcaodiogo <falcao.diogo@ua.pt>
2024-12-19 17:25:04 +0000

    Cleaned code of NavigationScreens and added support for the new screens

--- app/src/main/java/ua/diogo/cp/navigation/NavigationScreen.kt
+++ app/src/main/java/ua/diogo/cp/navigation/NavigationScreen.kt
@@ -1,42 +1,36 @@
 package ua.diogo.cp.navigation
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import ua.diogo.cp.ui.screens.ChatBotScreen
+import ua.diogo.cp.ui.screens.HomeScreen
+import ua.diogo.cp.ui.screens.SettingsScreen
+import ua.diogo.cp.ui.screens.StallmentsScreen
+import ua.diogo.cp.ui.screens.TrainsScreen
 
-@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Composable
 fun NavigationScreens(
     navController: NavHostController,
-    onSignOut: () -> Unit,
-//    googleAuthUiClient: GoogleAuthUiClient,
-//    context: Context,
-//    exerciseDao: ExerciseDao,
-//    userDao: UserDao
+    context: Context
 ) {
-//    val viewModel = DailyExerciseViewModel(
-//        exerciseDao = exerciseDao,
-//        googleAuthUiClient = googleAuthUiClient,
-//        userDao = userDao
-//    )
-
-//    NavHost(navController, startDestination = NavItem.Home.path) {
-//        composable(NavItem.Home.path) { googleAuthUiClient.getSignedInUser()
-//            ?.let { it1 -> HomeScreen(googleAuthUiClient, it1) } }
-//        composable(NavItem.Notifications.path) { NotificationsScreen(navController, context) }
-//        composable(NavItem.Sleep.path) { SleepScreen(context) }
-//        composable(NavItem.Settings.path) {
-//            val userData = googleAuthUiClient.getSignedInUser()
-//            ProfileScreen(userData, onSignOut, context, googleAuthUiClient)
-//        }
-//        composable(NavItem.Exercises.path) { PlannedExercises(navController) }
-//        composable(NavItem.Exercises.path + "/{day}") {backStackEntry ->
-//            val day = backStackEntry.arguments?.getString("day")
-//            DailyExercisesScreen(day, viewModel = viewModel, context)
-//        }
-//    }
+    NavHost(navController, startDestination = NavItem.Home.path) {
+        composable(NavItem.Home.path) {
+            HomeScreen()
+        }
+        composable(NavItem.Trains.path) {
+            TrainsScreen(context)
+        }
+        composable(NavItem.Stallments.path) {
+            StallmentsScreen(context)
+        }
+        composable(NavItem.ChatBot.path) {
+            ChatBotScreen(context)
+        }
+        composable(NavItem.Settings.path) {
+            SettingsScreen(context)
+        }
+    }
 }
