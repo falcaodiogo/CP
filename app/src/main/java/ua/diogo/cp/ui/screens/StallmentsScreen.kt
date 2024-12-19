08608b706d87ebbcfc451c19a4b2250464ab51ab
falcaodiogo <falcao.diogo@ua.pt>
2024-12-19 17:23:43 +0000

    New ChatBotScreen, SettingsScreen, StallmentsScreen and TrainsScreen

---
+++ app/src/main/java/ua/diogo/cp/ui/screens/StallmentsScreen.kt
@@ -0,0 +1,14 @@
+package ua.diogo.cp.ui.screens
+
+import android.content.Context
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StallmentsScreen(context: Context) {
+    Surface {
+        Text(text = "HomeScreen", modifier = Modifier)
+    }
+}
