9e7781a2e1da38ed5bf31fffecc276ac4832ce21
falcaodiogo <falcao.diogo@ua.pt>
2025-01-12 17:04:37 +0000

    Genderless homescreen

--- app/src/main/java/ua/diogo/cp/ui/components/CpLogo.kt
+++ app/src/main/java/ua/diogo/cp/ui/components/CpLogo.kt
@@ -9,14 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,7 +47,7 @@ fun CpLogo(
         )
         Spacer(modifier = Modifier.padding(16.dp))
         Text(
-            text = "Bem vindo, ${username2[0]}, à app renovada da CP - Comboios Portugal",
+            text = "Bem vind@, ${username2[0]}, à app renovada da CP - Comboios Portugal",
             color = MaterialTheme.colorScheme.onSurface,
             fontSize = MaterialTheme.typography.headlineSmall.fontSize,
             lineHeight = 28.sp,
