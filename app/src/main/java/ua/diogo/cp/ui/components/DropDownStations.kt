08f00a28e4aef04c603fc237097b6c314b8bf782
falcaodiogo <falcao.diogo@ua.pt>
2025-06-16 16:12:12 +0100

    Material 3 Expressive dropdown

--- app/src/main/java/ua/diogo/cp/ui/components/DropDownStations.kt
+++ app/src/main/java/ua/diogo/cp/ui/components/DropDownStations.kt
@@ -20,13 +20,9 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -41,8 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
@@ -52,7 +46,6 @@ import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 import ua.diogo.cp.data.retrofit.StationsViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DropDownStations(
     viewModel: StationsViewModel,
@@ -63,22 +56,25 @@ fun DropDownStations(
     var searchText by remember { mutableStateOf("") }
     var filteredStations by remember { mutableStateOf(stations) }
 
-    val focusRequester = remember { FocusRequester() }
-
     LaunchedEffect(Unit) {
         viewModel.fetchStations()
     }
 
     LaunchedEffect(searchText, stations) {
         filteredStations = if (searchText.isEmpty()) {
-            stations
+            stations.take(4)
         } else {
-            delay(800)
             stations.filter {
                 it.designation.contains(searchText, ignoreCase = true)
-            }
+            }.take(4)
         }
-        expanded = searchText.isNotEmpty()
+
+        if (searchText.isNotEmpty() && filteredStations.isNotEmpty()) {
+            delay(800)
+            expanded = true
+        } else {
+            expanded = false
+        }
     }
 
     Column(
@@ -96,92 +92,92 @@ fun DropDownStations(
             modifier = Modifier
                 .padding(8.dp)
         )
-        Row(
+
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(80.dp)
-                .shadow(3.dp, RoundedCornerShape(24.dp)),
-            horizontalArrangement = Arrangement.Center
+                .clip(RoundedCornerShape(24.dp))
         ) {
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = {},
+            TextField(
+                value = searchText,
+                onValueChange = { text ->
+                    searchText = text
+                },
+                placeholder = {
+                    Text(
+                        text = "Escolha uma estação",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    focusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                    unfocusedIndicatorColor = MaterialTheme.colorScheme.surface
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(8.dp))
-                    .shadow(6.dp, RoundedCornerShape(8.dp))
-            ) {
-                TextField(
-                    value = searchText,
-                    onValueChange = { text ->
-                        searchText = text
-                    },
-                    placeholder = {
-                        Text(
-                            text = "Escolha uma estação",
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Start
+                    .height(80.dp)
+                    .shadow(2.dp, RoundedCornerShape(24.dp))
+                    .background(Color.Transparent),
+                trailingIcon = {
+                    if (searchText.isNotEmpty()) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search",
+                            modifier = Modifier.clickable {
+                                val station = stations.find {
+                                    it.designation.equals(searchText, ignoreCase = true)
+                                }
+                                station?.let {
+                                    navController.navigate("stations/${it.code}")
+                                }
+                            }
                         )
-                    },
-                    colors = TextFieldDefaults.colors(
-                        focusedContainerColor = MaterialTheme.colorScheme.surface,
-                        unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                        focusedIndicatorColor = MaterialTheme.colorScheme.surface,
-                        unfocusedIndicatorColor = MaterialTheme.colorScheme.surface
-                    ),
-                    modifier = Modifier
-                        .menuAnchor()
-                        .fillMaxWidth()
-                        .height(80.dp)
-                        .shadow(6.dp, RoundedCornerShape(24.dp))
-                        .background(Color.Transparent)
-                        .focusRequester(focusRequester),
-                    trailingIcon = {
-                        if (searchText.isNotEmpty()) {
-                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-                        }
-                    },
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        imeAction = ImeAction.Search
-                    ),
-                    keyboardActions = KeyboardActions(onSearch = {
-                        expanded = false
-                    }),
-                    singleLine = true,
-                    textStyle = LocalTextStyle.current.copy(
-                        textAlign = TextAlign.Start,
-                        lineHeight = 80.sp
-                    )
+                    }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(onSearch = {
+                    expanded = false
+                    val station = stations.find {
+                        it.designation.equals(searchText, ignoreCase = true)
+                    }
+                    station?.let {
+                        navController.navigate("stations/${it.code}")
+                    }
+                }),
+                singleLine = true,
+                textStyle = LocalTextStyle.current.copy(
+                    textAlign = TextAlign.Start,
+                    lineHeight = 40.sp
                 )
+            )
 
-                if (expanded) {
-                    DropdownMenu(
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false },
-                        modifier = Modifier
-                            .width(300.dp)
-                            .clip(RoundedCornerShape(24.dp))
-                            .background(MaterialTheme.colorScheme.surface)
-                    ) {
-                        filteredStations.forEach { station ->
-                            DropdownMenuItem(
-                                text = {
-                                    Text(
-                                        text = station.designation,
-                                        color = MaterialTheme.colorScheme.onSurface
-                                    )
-                                },
-                                onClick = {
-                                    searchText = station.designation
-                                    expanded = false
-                                    navController.navigate("stations/${station.code}")
-                                },
-                                colors = MenuDefaults.itemColors(
-                                    disabledLeadingIconColor = MaterialTheme.colorScheme.onSurface,
-                                    disabledTrailingIconColor = MaterialTheme.colorScheme.onSurface
-                                ),
-                            )
-                        }
+            if (expanded) {
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    modifier = Modifier
+                        .width(300.dp)
+                        .shadow(elevation = 4.dp, shape = RoundedCornerShape(24.dp))
+                ) {
+                    filteredStations.forEach { station ->
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    text = station.designation,
+                                    color = MaterialTheme.colorScheme.onSurface
+                                )
+                            },
+                            onClick = {
+                                searchText = station.designation
+                                expanded = false
+                                navController.navigate("stations/${station.code}")
+                            }
+                        )
                     }
                 }
             }
@@ -202,47 +198,39 @@ fun DropDownStations(
                             "Escolha uma estação",
                             Toast.LENGTH_LONG
                         ).show()
+                    } else {
+                        val station = stations.find {
+                            it.designation.equals(searchText, ignoreCase = true)
+                        }
+                        if (station != null) {
+                            navController.navigate("stations/${station.code}")
+                        } else {
+                            Toast.makeText(
+                                navController.context,
+                                "Estação inválida",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
                     }
                 },
                 content = {
                     Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                val station = stations.find {
-                                    it.designation.equals(
-                                        searchText,
-                                        ignoreCase = true
-                                    )
-                                }
-                                if (station != null) {
-                                    navController.navigate("stations/${station.code}")
-                                } else {
-                                    Toast
-                                        .makeText(
-                                            navController.context,
-                                            "Estação inválida",
-                                            Toast.LENGTH_LONG
-                                        )
-                                        .show()
-                                }
-                            },
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
                     ) {
                         Text(
                             text = "Pesquisar",
                             fontSize = MaterialTheme.typography.bodyLarge.fontSize
                         )
                         Spacer(modifier = Modifier.width(12.dp))
-                        Icon(imageVector = Icons.Default.Search, contentDescription = "Pesquisar")
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Pesquisar"
+                        )
                     }
                 }
             )
         }
-    }
-
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
     }
 }
